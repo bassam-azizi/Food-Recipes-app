6d1ec290c2b72866c323504645213c38095f41dc
BassamAzizi <101010bassam@gmail.com>
2019-12-29 23:01:47 +0100

    fetching the data from the api and use their information

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Recipes from "./Recipes";
 
 const App = () => {
   const APP_ID = "3dad0141";
   const APP_KEY = "9999d40cef8531c5b99ef78414f3fbe6";
 
-  const exampleReq =`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data = await response.json();
+    setRecipes(data.hits);
+  };
 
   return (
     <div className="App">
       <form className="search-form">
         <input className="search-bar" type="text" />
-        <button className="search-button">Search</button>
+        <button className="search-button" type="submit">
+          Search
+        </button>
       </form>
+      {recipes.map(recipe => (
+        <Recipes
+          key={recipe.recipe.label}
+          title={recipe.recipe.label}
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image}
+        />
+      ))}
     </div>
   );
 };
