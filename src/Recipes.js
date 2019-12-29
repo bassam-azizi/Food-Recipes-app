5ad0c9ba498b6f2a6820d7a75ff472155adea3e8
BassamAzizi <101010bassam@gmail.com>
2019-12-30 00:26:14 +0100

    add the search resultat

--- src/Recipes.js
+++ src/Recipes.js
@@ -1,11 +1,21 @@
 import React from "react";
+import style from "./recipes.module.css";
 
-const Recipes = ({ title, calories, image }) => {
+const Recipes = ({ title, calories, image, ingredients }) => {
   return (
-    <div>
-      <h1>{title}</h1>
-      <p>{calories}</p>
-      <img src={image} alt="" />
+    <div className={style.recipes}>
+      <h1 className={style.title}>{title}</h1>
+      <div className={style.ingredient}>
+        <p>
+          <strong>Calories</strong> : {calories}
+        </p>
+        <ol className={style.list}>
+          {ingredients.map(ingredient => (
+            <li key={ingredient.name}>{ingredient.text}</li>
+          ))}
+        </ol>
+      </div>
+      <img className={style.image} src={image} alt="" />
     </div>
   );
 };
